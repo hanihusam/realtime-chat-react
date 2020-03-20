b2b76672210ff5ea840687353b8d086f414c906f
Hani Husamuddin <hani.husam@gmail.com>
2020-03-20 22:47:01 +0700

    Preparation for deployment to Heroku

--- server.js
+++ server.js
@@ -68,6 +68,16 @@ io.on("connect", socket => {
   });
 });
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on PORT ${PORT}`));
